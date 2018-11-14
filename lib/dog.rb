d3da715e7f4111342c0c90a70cdd6cac794a30f1
github username <github email address>
2018-11-14 09:32:57 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,4 @@
 class Dog
+  Dog.new = @this_dog_name
 
-  def name=(dogs_name)
-    @this_dogs_name = dogs_name
-  end
-
-  def name
-    @this_dogs_name
-  end
 end
-
-lassie = Dog.new
-lassie.name = "Lassie"
-
-puts lassie.name
